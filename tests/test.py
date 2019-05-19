fee08b519fe972901e8010e6d05970e3e96e5ef7
Daniil Kazantsev <dkazanc@hotmail.com>
2019-05-19 23:13:54 +0100

    tests added

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from tomobar.methodsDIR import RecToolsDIR
+from tomobar.methodsIR import RecToolsIR
+
+###############################################################################
+
+class TestRegularisers(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
