4a24f912152bf62e0c9449c9f66b2a393ffcba9e
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-08 21:19:11 +0000

    actions6

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -4,15 +4,16 @@ PKG_NAME=tomobar
 USER=dkazanc
 OS=linux-64
 CONDA_TOKEN=$(cat $HOME/secrets/my_secret.json)
-array=(3.5 3.6 3.7)
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
 conda install --yes anaconda-client
+conda build . --numpy=1.15 --python=3.5
+conda build . --numpy=1.15 --python=3.6
 conda build . --numpy=1.15 --python=3.7
-
+conda build . --numpy=1.18 --python=3.8
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
