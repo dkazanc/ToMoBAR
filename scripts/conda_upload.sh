6a85c9d96294ed89f52c191c268ee9b650d033d1
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-08 17:37:55 +0000

    actions1

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
-
 PKG_NAME=tomobar
 USER=dkazanc
 OS=linux-64
 CONDA_TOKEN=$(cat $HOME/secrets/my_secret.json)
+export VERSION=`date +%Y.%m`
+
+array=( 3.5 3.6 3.7 )
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m`
-conda install --yes anaconda-client
-conda build .
+
+# building conda packages
+for i in "${array[@]}"
+do
+	conda-build . --python $i $pkg
+done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
