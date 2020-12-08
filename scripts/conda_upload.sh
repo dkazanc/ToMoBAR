578ee244357b81641f71099ee9189e86db6795da
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-08 19:50:08 +0000

    actions5

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -6,12 +6,12 @@ OS=linux-64
 CONDA_TOKEN=$(cat $HOME/secrets/my_secret.json)
 array=(3.5 3.6 3.7)
 
-mmkdir ~/conda-bld
+mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
 conda install --yes anaconda-client
-conda build .
+conda build . --numpy=1.15 --python=3.7
 
 
 # upload packages to conda
