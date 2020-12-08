8bc9002af47f05cdc209329acf7392b014a8a31b
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-08 16:57:36 +0000

    adds github actions workflow

---
+++ scripts/conda_upload.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PKG_NAME=tomobar
+USER=dkazanc
+OS=linux-64
+CONDA_TOKEN=$(cat $HOME/secrets/my_secret.json)
+
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m`
+conda install --yes anaconda-client
+conda build .
+
+# upload packages to conda
+find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
+do
+    echo $file
+    $CONDA/bin/anaconda -v --show-traceback --token $CONDA_TOKEN upload $file --force
+done
