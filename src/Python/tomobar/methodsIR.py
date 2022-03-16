6af6e4a20e926ca81a4f3e0e8932a0c62b67d947
Daniil Kazantsev <dkazanc@hotmail.com>
2022-03-16 16:10:53 +0000

    control of GPU devices for PD_TV methods

--- src/Python/tomobar/methodsIR.py
+++ src/Python/tomobar/methodsIR.py
@@ -235,7 +235,7 @@ def prox_regul(self, X, _regularisation_):
         (X,info_vec) = FGP_TV(X, _regularisation_['regul_param'], _regularisation_['iterations'], _regularisation_['tolerance'], _regularisation_['methodTV'], self.nonneg_regul, _regularisation_['device_regulariser'])
     if 'PD_TV' in _regularisation_['method']:
         # Primal-Dual (PD) Total variation method by Chambolle-Pock
-        (X,info_vec) = PD_TV(X, _regularisation_['regul_param'], _regularisation_['iterations'], _regularisation_['tolerance'], _regularisation_['methodTV'], self.nonneg_regul, _regularisation_['PD_LipschitzConstant'], _regularisation_['device_regulariser'])
+        (X,info_vec) = PD_TV(X, _regularisation_['regul_param'], _regularisation_['iterations'], _regularisation_['tolerance'], _regularisation_['methodTV'], self.nonneg_regul, _regularisation_['PD_LipschitzConstant'], self.device_projector)
     if 'SB_TV' in _regularisation_['method']:
         # Split Bregman Total variation method
         (X,info_vec) = SB_TV(X, _regularisation_['regul_param'], _regularisation_['iterations'], _regularisation_['tolerance'], _regularisation_['methodTV'], _regularisation_['device_regulariser'])
