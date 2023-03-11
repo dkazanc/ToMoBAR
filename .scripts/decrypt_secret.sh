c34d0b0492be99a0eec63b8b7b200e9a6a267ef9
Daniil Kazantsev <dkazanc@hotmail.com>
2023-03-11 18:13:41 +0000

    actions6

--- .scripts/decrypt_secret.sh
+++ .scripts/decrypt_secret.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $HOME/.secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
