582fbe03658eb1a2f5e0eb382425508cdbca34c9
Daniil Kazantsev <dkazanc@hotmail.com>
2023-09-07 17:01:04 +0100

    return of the loop in conda upload

--- .scripts/conda_upload.sh
+++ .scripts/conda_upload.sh
@@ -9,8 +9,15 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 
-export VERSION=$(date +%Y.%m)
-$CONDA/bin/conda build .
+#export VERSION=$(date +%Y.%m)
+#$CONDA/bin/conda build .
+
+for python_ver in 3.9 3.10; do   
+    for numpy_ver in 1.21 1.22 1.23 1.24; do
+        export VERSION=$(date +%Y.%m)"_py"$python_ver"_np"$numpy_ver
+        conda build . --numpy $numpy_ver --python $python_ver
+    done
+done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
