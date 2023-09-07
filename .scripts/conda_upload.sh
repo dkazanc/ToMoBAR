0216bc8dacead020217ee0f05305e26ab1bb6ba0
Daniil Kazantsev <dkazanc@hotmail.com>
2023-09-07 14:51:09 +0100

    actions11

--- .scripts/conda_upload.sh
+++ .scripts/conda_upload.sh
@@ -9,11 +9,18 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 
-$CONDA/bin/conda install conda-build
-$CONDA/bin/conda install -c anaconda anaconda-client
+$CONDA/bin/conda install -c conda-forge conda-build
+$CONDA/bin/conda install -c conda-forge anaconda-client
 
-export VERSION=$(date +%Y.%m)
-conda build .
+#export VERSION=$(date +%Y.%m)
+#conda build .
+
+for python_ver in 3.9; do   
+    for numpy_ver in 1.21; do
+        export VERSION=`date +%Y.%m`"_py"$python_ver"_np"$numpy_ver
+        conda build . --numpy $numpy_ver --python $python_ver
+   done
+done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
