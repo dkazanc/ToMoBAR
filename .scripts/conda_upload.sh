436eafbfed93cf3e75385dc31818817be6269b04
Daniil Kazantsev <dkazanc@hotmail.com>
2023-12-07 22:33:10 +0000

    Update conda_upload.sh

--- .scripts/conda_upload.sh
+++ .scripts/conda_upload.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 PKG_NAME=tomobar
-USER=dkazanc
-OS=linux-64
+USER=httomo-team
+OS=noarch
 CONDA_TOKEN=$(cat $HOME/.secrets/my_secret.json)
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 
-#export VERSION=$(date +%Y.%m)
-#$CONDA/bin/conda build .
+$CONDA/bin/conda install conda-build
+$CONDA/bin/conda install -c anaconda anaconda-client
 
-for python_ver in 3.9 3.10; do   
-    for numpy_ver in 1.21 1.22 1.23 1.24; do
-        export VERSION=$(date +%Y.%m)"_py"$python_ver"_np"$numpy_ver
-        conda build . --numpy $numpy_ver --python $python_ver
-    done
-done
+$CONDA/bin/conda build .
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
